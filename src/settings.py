f558c7476324e02280e6f452e218fdec6cb5a916
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-08-14 19:12:35 +0600

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -15,6 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "Wind Turbine Detection (by Luke Borkowski)"
 PROJECT_NAME_FULL: str = "Wind Turbine Object Detection from Aerial Imagery Using TensorFlow Object Detection API and Google Colab"
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
@@ -79,6 +80,7 @@ def get_settings():
     settings = {
         "project_name": PROJECT_NAME,
         "license": LICENSE,
+        "hide_dataset": HIDE_DATASET,        
         "applications": APPLICATIONS,
         "category": CATEGORY,
         "cv_tasks": CV_TASKS,
